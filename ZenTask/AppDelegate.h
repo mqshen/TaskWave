711c67490551bd671cf9144da721b80dc4fdb4db
沈淼奇 <mqshen@126.com>
2014-05-25 22:26:16 +0800

    Initial Commit

---
+++ ZenTask/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ZenTask
+//
+//  Created by GoldRatio on 5/25/14.
+//  Copyright (c) 2014 GoldRatio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ZenTaskTests/ZenTaskTests.m
@@ -0,0 +1,34 @@
+//
+//  ZenTaskTests.m
+//  ZenTaskTests
+//
+//  Created by GoldRatio on 5/25/14.
+//  Copyright (c) 2014 GoldRatio. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ZenTaskTests : XCTestCase
+
+@end
+
+@implementation ZenTaskTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
